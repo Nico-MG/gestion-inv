b37b36c500d7996054e432d2353a24e5ada2cfae
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-04-15 11:38:07 -0400

    avance de db

---
+++ data.sql
@@ -0,0 +1,14 @@
+drop schema public cascade;
+create schema public;
+
+create table gerente (
+    id serial primary key
+);
+
+create table cliente (
+    id serial primary key
+);
+
+create table proveedor (
+    id serial primary key
+);
