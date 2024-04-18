8e1cc104bb3d1ac0da00156ff57053dc425aeb39
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-04-18 10:30:30 -0400

    react conectado

--- main.js
+++ main.js
@@ -7,13 +7,23 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-  },
+      contextIsolation: false,
+    }
   })
   win.loadURL('http://localhost:3000')
-  //win.loadFile('./public/index.html')
   pruebaExpress()
 }
 
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+
+  app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+      app.quit();
+  }
+});
