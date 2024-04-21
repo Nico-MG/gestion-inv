188aa038d682d2d8091c55880fb52749ed10c655
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-04-21 01:28:22 -0400

    router no conectado

--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,11 @@
-import { app, shell, BrowserWindow } from 'electron'
-import { createFileRoute, createURLRoute } from 'electron-router-dom'
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  const win = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
     show: false,
@@ -18,28 +17,22 @@ function createWindow () {
     }
   })
 
-  
-
-  win.on('ready-to-show', () => {
-    win.show()
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
   })
 
-  win.setFullScreen(false)
-
-  win.webContents.setWindowOpenHandler((details) => {
+  mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url)
     return { action: 'deny' }
   })
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env.ELECTRON_RENDERER_URL) {
-    win.loadURL(createURLRoute(process.env.ELECTRON_RENDERER_URL))
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
-    win.loadFile(createFileRoute(join(__dirname, '../renderer/index.html')))
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
-
-  
 }
 
 // This method will be called when Electron has finished
@@ -55,6 +48,9 @@ app.whenReady().then(() => {
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
+
+  // IPC test
+  ipcMain.on('ping', () => console.log('pong'))
 
   createWindow()
 
--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,15 +1,9 @@
-import Login from './layouts/Login'
-import Clientes from './layouts/Clientes'
-import { Router, Route } from 'electron-router-dom'
+
 
 function App () {
   return (
     <>
-      <h1>Hola mundo</h1>
-      <Router>
-        <Route path='/' Component={<Login/>} />
-        <Route path='/Clientes' Component={<Clientes/>} />
-      </Router>
+    <h1>Hola mundo</h1>
     </>
   )
 }
