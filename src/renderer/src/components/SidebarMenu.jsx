0323c40dba2271c6aed05d9896689cec70c97e3f
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-21 15:05:22 -0400

    change: Prettier to SidebarMenu.jsx

--- src/renderer/src/components/SidebarMenu.jsx
+++ src/renderer/src/components/SidebarMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./sidebarmenu.css";
-import logo_plus from "../images/logo_plus.png"
+import logo_plus from "../images/logo_plus.png";
 import i_producto from "../images/i_producto.png";
 import i_clientes from "../images/i_clientes.png";
 import i_proveedores from "../images/i_proveedores.png";
@@ -14,27 +14,82 @@ import i_ayuda from "../images/i_ayuda.png";
 import i_configuracion from "../images/i_configuracion.png";
 
 const SidebarMenu = () => {
-    return (
-        <div className="barra-lateral">
-            <img src={logo_plus} alt="logo_plus" className="logo" />
-            <div className="encabezado-barra-lateral">
-                <i className="fas fa-bars"></i> <span>MENÚ</span>
-            </div>
-            <ul className="menu">
-                <li><a href="#"><img src={i_producto} alt="Producto" className="icono" /> <span className="texto-opcion"> PRODUCTOS </span></a></li>
-                <li><a href="#"><img src={i_clientes} alt="Clientes" className="icono" /> <span className="texto-opcion"> CLIENTES </span></a></li>
-                <li><a href="#"><img src={i_proveedores} alt="Proveedores" className="icono" /> <span className="texto-opcion"> PROVEEDORES </span></a></li>
-                <li><a href="#"><img src={i_pedidos} alt="Pedidos" className="icono" /> <span className="texto-opcion">PEDIDOS</span></a></li>
-                <li><a href="#"><img src={i_historial} alt="Historial" className="icono" /> <span className="texto-opcion">HISTORIAL</span></a></li>
-                <li><a href="#"><img src={i_analitica} alt="Analista" className="icono" /> <span className="texto-opcion">ANALITÍCA</span></a></li>
-                <li><a href="#"><img src={i_alerta} alt="Alertas" className="icono" /> <span className="texto-opcion">ALERTAS</span></a></li>
-                <li><a href="#"><img src={i_devolucion} alt="Devolución" className="icono" /> <span className="texto-opcion">DEVOLUCIÓN</span></a></li>
-                <li><a href="#"><img src={i_informes} alt="Informes" className="icono" /> <span className="texto-opcion">INFORMES</span></a></li>
-                <li><a href="#"><img src={i_ayuda} alt="Ayuda" className="icono" /> <span className="texto-opcion">AYUDA Y SOPORTE</span></a></li>
-                <li><a href="#"><img src={i_configuracion} alt="Configuracion" className="icono" /> <span className="texto-opcion">CONFIGURACIÓN</span></a></li>
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div className="barra-lateral">
+      <img src={logo_plus} alt="logo_plus" className="logo" />
+      <div className="encabezado-barra-lateral">
+        <i className="fas fa-bars"></i> <span>MENÚ</span>
+      </div>
+      <ul className="menu">
+        <li>
+          <a href="#">
+            <img src={i_producto} alt="Producto" className="icono" />{" "}
+            <span className="texto-opcion"> PRODUCTOS </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_clientes} alt="Clientes" className="icono" />{" "}
+            <span className="texto-opcion"> CLIENTES </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_proveedores} alt="Proveedores" className="icono" />{" "}
+            <span className="texto-opcion"> PROVEEDORES </span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_pedidos} alt="Pedidos" className="icono" />{" "}
+            <span className="texto-opcion">PEDIDOS</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_historial} alt="Historial" className="icono" />{" "}
+            <span className="texto-opcion">HISTORIAL</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_analitica} alt="Analista" className="icono" />{" "}
+            <span className="texto-opcion">ANALITÍCA</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_alerta} alt="Alertas" className="icono" />{" "}
+            <span className="texto-opcion">ALERTAS</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_devolucion} alt="Devolución" className="icono" />{" "}
+            <span className="texto-opcion">DEVOLUCIÓN</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_informes} alt="Informes" className="icono" />{" "}
+            <span className="texto-opcion">INFORMES</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_ayuda} alt="Ayuda" className="icono" />{" "}
+            <span className="texto-opcion">AYUDA Y SOPORTE</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src={i_configuracion} alt="Configuracion" className="icono" />{" "}
+            <span className="texto-opcion">CONFIGURACIÓN</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default SidebarMenu;
