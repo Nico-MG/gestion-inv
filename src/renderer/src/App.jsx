375e423b1f557f583e4337595bab4ad6c38aea4d
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-23 23:36:22 -0400

    change: app.jsx to show login

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import Sidebar from "./components/Organisms/Menus/Sidebar";
-import Productos from "./components/Pages/Productos";
-import Pedidos from "./components/Pages/Pedidos";
 import Login from "./components/Organisms/Forms/Login/Login";
-import AddButton from "./components/Molecules/TableButtons/Add/AddButton";
 
 const App = () => {
   return (
     <>
-      <Pedidos />
+      <Login />
     </>
   );
 };
