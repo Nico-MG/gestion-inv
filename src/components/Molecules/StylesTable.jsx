3aaabcd11ceaf4cb3d779708d5046979a09d4cd0
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-28 16:20:01 -0400

    change: table.css to always show buttons when hovering on row

---
+++ src/components/Molecules/StylesTable.jsx
@@ -0,0 +1,24 @@
+import { styled } from "@mui/material/styles";
+import TableRow from "@mui/material/TableRow";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#d9d9d9",
+    color: theme.palette.common.black,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  // '&:nth-of-type(odd)': {
+  //   backgroundColor: theme.palette.action.hover,
+  // },
+  backgroundColor: "white",
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
