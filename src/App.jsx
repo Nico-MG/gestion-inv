ebc3eabca3fe94ef87dc0eea459f1303da802435
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-29 23:20:46 -0400

    change: MUISidebar in process

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./components/Organisms/Forms/Login/Login";
+import MUISidebar from "./components/Organisms/Menus/MUISidebar";
 
 const App = () => {
   return (
     <>
       <Login />
+      {/* <MUISidebar /> */}
     </>
   );
 };
