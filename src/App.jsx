c7aaad98836edc68e49f9253f9c72e9a738fd89a
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-30 02:01:43 -0400

    change: App.jsx to only show login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Login from "./components/Organisms/Forms/Login/Login";
-// import MUISidebar from "./components/Organisms/Menus/MUISidebar";
 
 const App = () => {
   return (
     <>
       <Login />
-      {/* <MUISidebar /> */}
     </>
   );
 };
