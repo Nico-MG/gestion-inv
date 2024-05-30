bc8653f267bb02696fe391c79ff589b4e20f1439
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-05-30 07:00:44 -0400

    change: new style for login

--- src/components/Organisms/Forms/Login/Login.jsx
+++ src/components/Organisms/Forms/Login/Login.jsx
@@ -1,51 +1,143 @@
-/*LOGIN ver. 0.2*/
 import { useState } from "react";
-import "./login.css";
-import logo_plus from "../../../../images/logo_plus.png";
 import UserApi from "../../../../services/Api/user.service";
+import {
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
 import Sidebar from '../../Menus/Sidebar';
 
-const T2 = ({handleChange, handleSubmit}) => {
-    return (
-	<>
-	  <div className="container">
-      <div className="sombra-azul"></div>
-     
-      <div className="izquierda">
-        <img src={logo_plus} />
-      </div>
-     
-      <div className="derecha">
-        <h3>¡BIENVENIDO!</h3>
-        <h4>Accede a tu cuenta</h4>
-        <form onSubmit={handleSubmit}>
-          
-          <input 
-            name="rut" 
-            type="text" 
-            placeholder="RUT" 
-            onChange={handleChange}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Contrasena"
-            onChange={handleChange}
-          />
-          
-          <div className="checkbox-container">
-            <input type="checkbox" id="recordar" name="Recordar" value="0" />
-            <label htmlFor="recordar">Recordar usuario</label>
-          </div>
-        
-          <button type="submit">Ingresar</button>
-        
-        </form>
-      </div>
-    </div>  
-	</>
+const StyledTextField = styled(TextField)({
+  marginBottom: "2vh",
+  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    borderColor: "#9bc661",
+  },
+  "& .MuiInputLabel-outlined.Mui-focused": {
+    color: "#9bc661",
+  },
+});
 
-    )}
+const T2 = ({handleChange, handleSubmit}) => {
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 9999,
+        display: "flex",
+      }}
+    >
+      <Box
+        sx={{
+          flex: 1,
+          backgroundColor: "#266763",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.6)",
+          zIndex: 1,
+        }}
+      >
+        <CardMedia
+          component="img"
+          alt="StockBox"
+          image="/src/images/logo_2.png"
+          sx={{
+            width: "auto",
+            maxWidth: "55%",
+          }}
+        />
+      </Box>
+
+      <Box
+        sx={{
+          flex: 1,
+          backgroundColor: "#fff",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Card
+          sx={{
+            width: "65%",
+            borderRadius: "24px",
+            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <Box sx={{ padding: "50px", margin: 2 }}>
+            <Typography
+              variant="h3"
+              sx={{ fontWeight: "bold", textAlign: "center" }}
+            >
+              ¡BIENVENIDO!
+            </Typography>
+            <Typography variant="h4" sx={{ textAlign: "center" }}>
+              ACCEDE A TU CUENTA
+            </Typography>
+          </Box>
+
+          <form onSubmit={handleSubmit}>
+            <Box
+              sx={{
+                width: "65%",
+                margin: "auto",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Box>
+                <StyledTextField label="RUT" name="rut" fullWidth onChange={handleChange} />
+                <StyledTextField label="Contraseña" name="password" onChange={handleChange} fullWidth />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      sx={{
+                        color: "#348d87",
+                        "&.Mui-checked": {
+                          color: "#348d87",
+                        },
+                      }}
+                    />
+                  }
+                  label="Recordar usuario"
+                />
+              </Box>
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#266763",
+                  color: "#ffffff",
+                  fontSize: "1rem",
+                  borderRadius: "12px",
+                  "&:hover": {
+                    backgroundColor: "#c3fa7b",
+                    color: "#7e7e7e",
+                  },
+                  margin: 5,
+                }}
+                type="submit"
+              >
+                INGRESAR
+              </Button>
+            </Box>
+          </form>
+        </Card>
+      </Box>
+    </Box>
+  );}
 
 const Login = () => {
   const [logged, setLogged] = useState("Login");
