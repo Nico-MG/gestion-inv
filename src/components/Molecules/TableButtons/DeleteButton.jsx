969e6470701fc757ff1cbbee6135ee315acc4c90
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-06-01 21:46:45 -0400

    add: alert in delete, plus catch error

--- src/components/Molecules/TableButtons/DeleteButton.jsx
+++ src/components/Molecules/TableButtons/DeleteButton.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import AuxDelete from "./AuxDelete";
+import { auxDelete } from "../../../functions/auxDelete";
 
 const DeleteButton = ({ currentTable, id, fetchData }) => {
-  const deleteTableRow = AuxDelete({ currentTable });
+  const deleteTableRow = auxDelete({ currentTable });
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteTableRow(id);
+      await fetchData();
+      alert(`Eliminado producto con ID: ${id}`);
+    }
+    catch (error) {
+      alert(`Error al eliminar producto: ${error}`)
+    }
+  }
 
   return (
     <IconButton
-      onClick={() => deleteTableRow(id).then(() => fetchData())}
+      onClick={() => handleDelete(id)}
       sx={{
         width: 32,
         height: 32,
