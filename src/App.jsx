58f27f0beef2edebd3728c6e08222f6da3a86b30
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-06-02 17:22:42 -0400

    change: App.jsx to show sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Login from "./components/Organisms/Forms/Login";
+import Sidebar from "./components/Organisms/Sidebar"
 
 const App = () => {
   return (
     <>
-      <Login />
+      <Sidebar />
     </>
   );
 };
