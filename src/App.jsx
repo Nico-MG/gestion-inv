422255f92038c853a25e44dd944d219114c3d3db
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-06-01 21:35:13 -0400

    fix validator product

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Login from "./components/Organisms/Forms/Login/Login";
 import MockupOrder from "./components/Organisms/Forms/Order/MockupOrder"
+import Sidebar from "./components/Organisms/Menus/Sidebar";
 
 const App = () => {
   return (
     <>
-      <Login />
+      <Sidebar />
       
     </>
   );
