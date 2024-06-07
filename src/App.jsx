342c96462f9546eecd7a2755e8caccfc5dba4338
Sebastian01Molina <sebastianosvaldo.molina@alumnos.ulagos.cl>
2024-06-07 10:48:46 -0400

    form order actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./components/Organisms/Forms/Login";
 import Sidebar from "./components/Organisms/Sidebar"
+import Mockup_OrderForm from "./components/Organisms/Forms/MockupOrder";
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      {/*<Mockup_OrderForm/>*/}
     </>
   );
 };
