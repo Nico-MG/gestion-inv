f565ed3404a9e3acedd722ff4de21e498f4f16a5
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-06-09 21:41:18 -0400

    added mockuporder with upgrades as mockuporderv3

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { Grid } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { lightTheme, darkTheme } from "./theme/theme";
 import Login from "./components/Organisms/forms/Login";
+import Mockup_OrderForm from "./components/organisms/forms/MockupOrderV3";
+import MockupOrderV2 from "./components/organisms/forms/MockupOrderV2";
+import ProductForm from "./components/organisms/forms/ProductForm";
 
 const App = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <Grid container sx={{ minHeight: "100vh", minWidth: "720px" }}>
-        <Login />
+        {/* <Login /> */}
+        <Mockup_OrderForm />
+        {/* <MockupOrderV2 /> */}
+        {/* <ProductForm /> */}
       </Grid>
     </ThemeProvider>
   );
