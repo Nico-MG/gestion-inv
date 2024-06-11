cadc8a2e3d841abaa8fd9665bbefe4d5148205ec
Katia Arevalo <katiaalejandra.arevalo@alumnos.ulagos.cl>
2024-06-11 19:15:32 -0400

    Create: OrderDetail Modal.

---
+++ src/components/Organisms/modals/OrderDetails.jsx
@@ -0,0 +1,25 @@
+// Importar aquí. 
+
+const detailData = {
+    id_pedido: 1,
+    rut_proveedor: "12345678-9",
+    rut_usuario: "98765432-1",
+    fecha: "2023-06-01",
+    compra_total: 150,
+    detalle_pedido: [
+      {
+        id_pedido: 1,
+        id_producto: 101,
+        cantidad: 2,
+        precio_unidad: 50,
+        precio_total: 100,
+      },
+      {
+        id_pedido: 1,
+        id_producto: 102,
+        cantidad: 1,
+        precio_unidad: 50,
+        precio_total: 50,
+      },
+    ],
+}
