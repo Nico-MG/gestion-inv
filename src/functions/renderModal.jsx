754b117324ec4a63c1ccaa9e2eda0b22520309a5
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-06-12 18:57:27 -0400

    added: stashed changes

--- src/functions/renderModal.jsx
+++ src/functions/renderModal.jsx
@@ -1,8 +1,11 @@
 import SaleDetails from "../components/organisms/modals/SaleDetails";
+import OrderDetails from "../components/organisms/modals/OrderDetails";
 
 const RenderModal = ({ currentTable, modalProps }) => {
   switch (currentTable) {
     case "orders":
+      return <OrderDetails {...modalProps} />;
+    case "sales":
       return <SaleDetails {...modalProps} />;
     default:
       return null;
